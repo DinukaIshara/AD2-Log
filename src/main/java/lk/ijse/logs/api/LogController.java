12ef1117e76a37e23ace3332bff7af9a6f30fff2
oshivimarsha <oshadivimarsha@gmail.com>
2025-05-21 07:02:29 +0530

    Update Project

--- src/main/java/lk/ijse/logs/api/LogController.java
+++ src/main/java/lk/ijse/logs/api/LogController.java
@@ -1,13 +1,31 @@
 package lk.ijse.logs.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogController {
+    Logger logger = LoggerFactory.getLogger(LogController.class);
+
     @GetMapping("/log")
     public String logMessage() {
-        return "Log";
+        logger.debug("This is DEBUG log message");
+        logger.info("This is INFO log message"); // sout wage dano nm meka use karanna
+        logger.warn("This is WARN log message");
+        logger.error("This is ERROR log message");
+
+        // Log Management
+        // ELK stack, Logstash, kibana
+
+        try {
+            String notNumberStr = "hello";
+            int number = Integer.parseInt(notNumberStr);
+        } catch (NumberFormatException e) {
+            logger.error("This is number format error : {}", e.getMessage());
+        }
+        return "This is sample end point for test log management";
     }
 }
